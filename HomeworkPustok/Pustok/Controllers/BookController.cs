b2e7ddcb8078c771ca04ff789b9d9bb704535f99
unknown <magsudrm@code.edu.az>
2023-04-16 21:17:32 +0400

    Neyse

--- HomeworkPustok/Pustok/Controllers/BookController.cs
+++ HomeworkPustok/Pustok/Controllers/BookController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Pustok.DAL;
+using Pustok.Models;
 
 namespace Pustok.Controllers
 {
@@ -11,6 +12,17 @@ namespace Pustok.Controllers
         public BookController(PustokDbContext context)
         {
             _context = context;
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var book = _context.Books
+            .Include(x => x.BookImages)
+            .Include(x => x.Genre)
+            .Include(x => x.BookTags)
+            .Include(x => x.Author)
+            .FirstOrDefault(x => x.Id == id);
+            return View(book);
         }
 
         public IActionResult GetBookModal(int id)
